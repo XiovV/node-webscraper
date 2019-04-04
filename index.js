2fb07005086bb2e336edf95f29d86195d64e6963
xiovv <xiovv@tutanota.com>
2019-04-04 14:29:58 +0200

    code refactor and TODOs

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const argv = require('minimist')(process.argv.slice(1));
+const argv = require('minimist')(process.argv.slice(2));
 
-// get the -c command line argument
+// the -c argument stands for channel. ie. -c https://youtube.com/user/ChannelName
 const url = argv.c;
+// the -f argument is a boolean value. If the app is being run for the first time then it needs to be set to true. ie. -f true
+const first = argv.f;
+
+// TODO: read from json file
+const latestVideoDownloaded = "http://www.youtube.com/watch?v=Q1kmrl9s8u";
 
 function getData(url) {
 	request(url, (err, res, html) => {
@@ -23,8 +28,22 @@ function jsonParse(string) {
 		const jsonData = JSON.parse(string);
 		const latestVideo = jsonData.itemListElement[0].item.itemListElement[0].url;
 		console.log(latestVideo);
+		
+		downloadVideo(latestVideo, first);
 	} catch(e) {
 		console.log("Try again... TODO: FIX THIS PROBLEM");
+	}
+}
+
+function downloadVideo(videoUrl, first) {
+	if(first === "true") { // If -f is true then write videoUrl to a json file and download the audio/video
+		// TODO: write videoUrl to a json file
+		// TODO: download video/audio
+		console.log("first time running.. download the video");
+	} else if(videoUrl == latestVideoDownloaded && first === "false") { // If videoUrl is the same as latestVideo then don't download
+		console.log("do not dl")
+	} else if(videoUrl != latestVideoDownloaded && first == "false"){ // If videoUrl is not the same as latestVideo then download
+		console.log("download");
 	}
 }
 
