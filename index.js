6ab088d388cbeeb7e8b282c01ea9cca665496cfe
xiovv <xiovv@tutanota.com>
2019-04-04 15:46:45 +0200

    if -f is not specified then the script will treat it as if it were set to false

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const url = argv.c;
 const first = argv.f;
 
 // TODO: use fs.readFile
-const latestVideoDownloaded = require('./latest_downloaded.json');
+const latestVideoDownloaded = require('./latest_downloaded.json').latestVideoDownloaded;
 
 function getData(url) {
 	request(url, (err, res, html) => {
@@ -28,7 +28,7 @@ function jsonParse(string) {
 	try {
 		const jsonData = JSON.parse(string);
 		const latestVideo = jsonData.itemListElement[0].item.itemListElement[0].url;
-		console.log(`latestVideo: ${latestVideo} \n latestVideoDownloaded:$ ${latestVideoDownloaded.latestVideoDownloaded}`);
+		console.log(`latestVideo: ${latestVideo} \n latestVideoDownloaded:$ ${latestVideoDownloaded}`);
 		
 		downloadVideo(latestVideo, first);
 	} catch(e) {
@@ -38,14 +38,13 @@ function jsonParse(string) {
 
 function downloadVideo(videoUrl, first) {
 	if(first === "true") { // If -f is true then write videoUrl to a json file and download the audio/video
-		// TODO: write videoUrl to a json file
 		writeLatestVideoDownloaded(videoUrl);
 		// TODO: download video/audio
 		console.log("first time running.. download the video");
-	} else if(videoUrl == latestVideoDownloaded.latestVideoDownloaded && first === "false") { // If videoUrl is the same as latestVideo then don't download
+	} else if(videoUrl == latestVideoDownloaded && (first === "false" || first == undefined)) { // If videoUrl is the same as latestVideo then don't download
 		writeLatestVideoDownloaded(videoUrl);
 		console.log("do not dl")
-	} else if(videoUrl != latestVideoDownloaded.latestVideoDownloaded && first == "false"){ // If videoUrl is not the same as latestVideo then download
+	} else if(videoUrl != latestVideoDownloaded && (first == "false" || first == undefined)){ // If videoUrl is not the same as latestVideo then download
 		writeLatestVideoDownloaded(videoUrl);
 		console.log("download");
 	}
