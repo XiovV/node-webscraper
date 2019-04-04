4929eec35127882fd6261dfa7b8965b7e1683d9f
xiovv <xiovv@tutanota.com>
2019-04-04 13:41:43 +0200

    added some comments and refactored the code a bit

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const request = require('request');
 const cheerio = require('cheerio');
 const argv = require('minimist')(process.argv.slice(1));
 
+// get the -c command line argument
 const url = argv.c;
 
 function getData(url) {
 	request(url, (err, res, html) => {
 		if(!err && res.statusCode == 200) {
 			const $ = cheerio.load(html);
-
+			
+			// gets json data from a script tag. Note: this sometimes causes JSON.parse to crash the app.
 			const data = $('script').get()[8].children[0].data;
 
 			jsonParse(data);
@@ -19,7 +21,8 @@ function getData(url) {
 function jsonParse(string) {
 	try {
 		const jsonData = JSON.parse(string);
-		console.log(jsonData.itemListElement[0].item.itemListElement[0].url);
+		const latestVideo = jsonData.itemListElement[0].item.itemListElement[0].url;
+		console.log(latestVideo);
 	} catch(e) {
 		console.log("Try again... TODO: FIX THIS PROBLEM");
 	}
