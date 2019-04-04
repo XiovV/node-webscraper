da255a13902cb7f312aa1aea88b82bd9505847ea
xiovv <xiovv@tutanota.com>
2019-04-04 21:18:03 +0200

    implemented -i argument

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const youtubedl = require('youtube-dl');
 const url = argv.c;
 // the -f argument is a boolean value. If the app is being run for the first time then it needs to be set to true. ie. -f true
 const first = argv.f;
+// the -i argument specifies a setTimeout interval
+const interval = argv.i;
 let fileName = "";
 // TODO: use fs.readFile
 const latestVideoDownloaded = require('./latest_downloaded.json').latestVideoDownloaded;
@@ -20,13 +22,13 @@ function getData(url) {
 			// gets json data from a script tag. Note: this sometimes causes JSON.parse to crash the app.
 			const data = $('script').get()[8].children[0].data;
 			
-			jsonParse(data);
+			jsonParse(data, url);
 		}
 	});
 
 }
 
-function jsonParse(string) {
+function jsonParse(string, url) {
 	// TODO: Fix infinite loop
 	while(true) {
 		try {
@@ -37,23 +39,24 @@ function jsonParse(string) {
 			break;
 		} catch(e) {
 			console.log("There was an error. Trying again...");
+			getData(url);
 		}
 	}
 }
 
 function downloadVideo(videoUrl, first) {
-	if(first === "true") { // If -f is true then write videoUrl to a json file and download the audio/video
+	if((first === "true") || (videoUrl != latestVideoDownloaded && (first == "false" || first == undefined))) { // If -f is true then write videoUrl to a json file and download the audio/video
 		writeLatestVideoDownloaded(videoUrl);
 		getVideoData(videoUrl);
 	} else if(videoUrl == latestVideoDownloaded && (first === "false" || first == undefined)) { // If videoUrl is the same as latestVideo then don't download
-		console.log("do not dl");
-		setTimeout(getData, 2000, url);
-	} else if(videoUrl != latestVideoDownloaded && (first == "false" || first == undefined)){ // If videoUrl is not the same as latestVideo then download
-		writeLatestVideoDownloaded(videoUrl);
-		console.log("download");
+		console.log("No new videos detected");
+		if(interval != undefined) {
+			setTimeout(getData, interval, url);
+		} else if (interval == undefined){
+			setTimeout(getData, 10000, url);
+		}
 	}
 }
-
 
 async function getVideoData(videoUrl) {
 	let video = await youtubedl(videoUrl);
